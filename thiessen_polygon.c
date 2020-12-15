42b81fc0c90b3efaf8a340375454cdd8db088ae6
Lucky-SARMs <lockheedx15@outlook.com>
2020-12-15 02:14:13 -0700

    replaced magic numbers in thiessen_polygon.c

--- thiessen_polygon.c
+++ thiessen_polygon.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <malloc.h>
 
+#define INDEX_MIN 0
 #define WIDTH 5
 #define HEIGHT 3
 #define MAX_ARRAY 20
@@ -10,7 +11,7 @@ void set_array(char *char_ptr, int width_limit, int height_limit);
 void print_array(char *char_ptr, int width_limit, int height_limit);
 
 int main(void){
-    char *character_array = malloc(15*sizeof(int));
+    char *character_array = malloc(WIDTH*HEIGHT*sizeof(int));
     set_array(character_array, WIDTH, HEIGHT);
     print_array(character_array, WIDTH, HEIGHT);
     free(character_array);
@@ -19,15 +20,15 @@ int main(void){
 
 void set_array(char *char_ptr, int width_limit, int height_limit){
     int max_index = (width_limit * height_limit);
-    for(int counter = 0; counter < max_index; counter++){
+    for(int counter = INDEX_MIN; counter < max_index; counter++){
         (*(char_ptr + (counter * sizeof(char)))) = 'x';
     }
 }
 
 void print_array(char *char_ptr, int width_limit, int height_limit){
     int max_index = (width_limit * height_limit);
-    for(int counter = 0; counter < max_index; counter++){
-        if(counter%WIDTH == 0){
+    for(int counter = INDEX_MIN; counter < max_index; counter++){
+        if(counter%WIDTH == INDEX_MIN){
             printf("\n");
         }
         printf("%c", *(char_ptr + counter*sizeof(char)));
